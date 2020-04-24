bb48d83a524782812849af9c844d024d277831c7
luke john <luke0145555@hotmail.co.uk>
2020-04-24 19:35:26 +0100

    toggle and change input values based on input properties

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from 'framer-motion'
 import { Card, CardGrid, Container, Header } from "./Elements";
 import "./App.css";
@@ -10,30 +10,32 @@ import green from "./green.png";
 
 //by default all transforms are 3d.
 // you should only animate transforms and opacity
-// shortcuts x y 
+// translate shortcuts x y z
+// translate : translateX, translateY, translateZ
+// Scale: scale, scaleX, scaleY
+// Rotate: rotate, rotateX, rotateY, rotateZ
+// Skew: skew, skewX, skewY
+
 function App() {
+  const [value, setValue] = useState(0)
+  const [isToggled, setToggle] = useState(1)
   return (
-    <motion.div 
-    initial={{ 
-      opacity: 0,
-      x: 100,
-      y: 100
-    }}
-    animate={{ 
-      opacity: 1,
-      x: 0,
-      y: 0
-    }}
-    transition={{
-      duration: 5
-    }}
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: isToggled }}
+      transition={{ duration: 5 }}
     >
       <Header>
         <Menu />
         <h1>Header</h1>
       </Header>
       <Container>
-        <h2>Super Cool</h2>
+        <motion.h2 animate={{ opacity: isToggled, x: value * 10 + "px" }}>Super Cool</motion.h2>
+        <button onClick={() =>
+          setToggle((prevValue) =>  prevValue ? 0 : 1 )}>
+          Toggle
+        </button>
+        <input type="range" min="-100" max="100" value={value} onChange={(e) => setValue(e.target.value)}></input>
         <CardGrid>
           <Card style={{ background: "var(--purp)" }}>
             <h3>Some card</h3>
